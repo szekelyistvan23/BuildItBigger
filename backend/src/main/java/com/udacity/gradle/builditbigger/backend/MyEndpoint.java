3f3327769c0de3edc4b7889a933de8045f9841eb
Szekely Istvan <steven.sekler@gmail.com>
2018-05-05 17:37:47 +0300

    feat: Jokes added

--- backend/src/main/java/com/udacity/gradle/builditbigger/backend/MyEndpoint.java
+++ backend/src/main/java/com/udacity/gradle/builditbigger/backend/MyEndpoint.java
@@ -1,5 +1,6 @@
 package com.udacity.gradle.builditbigger.backend;
 
+import com.example.jokeprovider.JokeProvider;
 import com.google.api.server.spi.config.Api;
 import com.google.api.server.spi.config.ApiMethod;
 import com.google.api.server.spi.config.ApiNamespace;
@@ -23,7 +24,13 @@ public class MyEndpoint {
     public MyBean sayHi(@Named("name") String name) {
         MyBean response = new MyBean();
         response.setData("Hi, " + name);
+        return response;
+    }
 
+    @ApiMethod(name = "getJoke")
+    public MyBean getJoke() {
+        MyBean response = new MyBean();
+        response.setData(JokeProvider.getOneJoke());
         return response;
     }
 
--- jokeprovider/src/main/java/com/example/jokeprovider/JokeProvider.java
+++ jokeprovider/src/main/java/com/example/jokeprovider/JokeProvider.java
@@ -1,4 +1,32 @@
 package com.example.jokeprovider;
 
+import java.util.Random;
+
 public class JokeProvider {
+    public static String[] jokes = {"Q: What do elves learn at school?\n" +
+            "A: The elf-abet.",
+            "Q: Why was the math book sad?\n"
+                    + "A: Because it had so many problems.",
+            "Q: What did the calculator say to the math student?\n" +
+                    "A: You can count on me.",
+            "Q: Why did the boy bring the ladder to school?\n" +
+                    "A: He was going to high school.",
+            "Q: Why were the teacher's eyes crossed?\n" +
+                    "A: She couldn't control her pupils.",
+            "Q: Why was the student's report card wet?\n" +
+                    "A: It was below C level!",
+            "Q: What three candies can you find in every school?\n" +
+                    "A: Nerds, DumDums, and Smarties.",
+            "Q: What did the buffalo say to his kid when he dropped him off at school?\n" +
+                    "A: Bison (as in, \"bye, son\")!",
+            "Q: What's a snake's favorite subject?\n" +
+                    "A: Hisstory. ",
+            "Q: Why did the teacher wear sunglasses inside?\n" +
+                    "A: Her students were so bright!"};
+
+    public static String getOneJoke() {
+        Random random = new Random();
+        int index  = random.nextInt(jokes.length);
+        return jokes[index];
+    }
 }
